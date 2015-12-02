b400ab9b29a7a8ec6a8f5c2c32071ab1f38ee0a2
dalecgu <131250169@smail.nju.edu.cn>
2015-12-02 14:47:17 +0800

    wm反馈

--- app/UserDoctor.php
+++ app/UserDoctor.php
@@ -8,4 +8,9 @@ class UserDoctor extends Model
 {
     protected $table = 'user_doctor';
     protected $fillable = ['user_id', 'doctor_id'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
 }
